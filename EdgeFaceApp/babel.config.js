a3ea261c221edfc44d8e89d483f11cb040e9191a
AYUSH26236 <AYUSHTIWARI7005@GMAIL.COM>
2026-06-01 16:36:35 +0530

    yug

--- EdgeFaceApp/babel.config.js
+++ EdgeFaceApp/babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
     'react-native-worklets-core/plugin',
   ],
 };
